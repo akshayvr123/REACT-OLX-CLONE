61a2c39802447cb194d2f7e68bb5882a6afc6504
Akshay Vr <avr02339@gmail.com>
2023-03-08 20:19:42 +0530

    NetFlix Design Completed video-6

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Banner.css"
+function Banner() {
+  return (
+    <div className='banner'>
+         <div className='content'>
+         <h1 className='title'>Movie Name</h1>
+         <div className='banner-buttons'>
+            <button className='button'>Play</button>
+            <button className='button'>My list</button>
+         </div>
+            <h1 className='Description'>Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h1>
+         </div>
+         <div className='fade-bottom'>
+            
+         </div>
+    </div>
+  )
+}
+
+export default Banner
+//22 min watched
