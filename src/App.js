d21171bc4109c1bccd44f8ace5fd2c53f0652d9b
Akshay Vr <avr02339@gmail.com>
2023-03-11 09:31:24 +0530

    Router completed with link and button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import RowPost from './Components/RowPost/RowPost';
-import "./App.css"
 import { useState } from 'react';
-import Banner from './Components/Banner/Banner';
-import NavBar from './Components/NavBar/NavBar';
+import About from './Container/About';
+import Profile from './Container/Profile';
+import NotFound from './Container/NotFound';
+import {
+  Route,Routes,Link, useNavigate
+} from "react-router-dom";
+
 function App() {
- 
+  const navigate = useNavigate();
   return (
     <div className="app">
-     <NavBar/>
-     <Banner/>
-     <RowPost/>
+      <button onClick={() => navigate('/about')}>Click</button>
+      <ul>
+        <li> <Link to='/about'>Home</Link> </li>
+        <li> <Link to='/profile'>Profile</Link> </li>
+       
+      </ul>
+         <Routes>
+         <Route path 
+            ='*' element={<NotFound></NotFound>}></Route>
+            <Route path 
+            ='/about' element={<About></About>}></Route>
+             <Route path 
+            ='/profile' element={<Profile></Profile>}></Route>
+         </Routes>
+         
     </div>
   );
 }
